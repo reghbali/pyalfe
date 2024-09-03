247551534980f3ad4920bad65e3041de1be604a3
Reza <reza.eghbali@ucsf.edu>
2024-09-03 13:07:26 -0700

    fixed integration test assert

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -95,7 +95,7 @@ class TestIntegration(TestCase):
                 msg=f'{summary_quantification_path} does not exist.',
             )
             summary_quantification = pd.read_csv(summary_quantification_path)
-            self.assertEqual(summary_quantification.dropna().shape, (63, 2))
+            self.assertEqual(summary_quantification.dropna().shape, (65, 2))
 
             individual_quantification = pd.read_csv(individual_quantification_path)
-            self.assertEqual(individual_quantification.dropna().shape[1], 61)
+            self.assertEqual(individual_quantification.dropna().shape[1], 63)
