58235761d107e1a709c47fd0d4fdd1a8948b4cd5
Reza <reza.eghbali@ucsf.edu>
2024-09-10 11:21:22 -0700

    added missing self return

--- pyalfe/interfaces/freesurfer.py
+++ pyalfe/interfaces/freesurfer.py
@@ -7,6 +7,7 @@ class FreeSurfer:
 
     def mri_synthseg(self, input, output):
         self.cmd += ['mri_synthseg', '--i', input, '--o', output]
+        return self
 
     def run(self):
         return subprocess.run(self.cmd, capture_output=True).stdout.decode("utf-8")
